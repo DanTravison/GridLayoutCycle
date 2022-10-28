91b4ceb2d3cfdf3067e98bbdc09e9dea285e53de
Dan Travison <Dan.Travison@live.com>
2022-10-28 10:16:03 -0700

    Initial repro example.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SliderBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SliderBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
